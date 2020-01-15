21cde8fdbe688781bc6bfa236c350d2f2fcca4e2
Julio-Maldonado <juliom@Julios-MacBook-Pro-3.local>
2020-01-14 23:19:26 -0800

    Updates

---
+++ precache-manifest.f7b73f6cbc6afe598b352b98ada8bbe3.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e45c70e58bb9e5a8236d4527dbe425",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f7907c5406fe74d0b4c",
+    "url": "/static/css/3.6b280986.chunk.css"
+  },
+  {
+    "revision": "55e50bf75b99e612d9f0",
+    "url": "/static/css/4.72a580bc.chunk.css"
+  },
+  {
+    "revision": "ac0df1bad76f52cccb2c",
+    "url": "/static/js/2.1e3e61db.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/2.1e3e61db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f7907c5406fe74d0b4c",
+    "url": "/static/js/3.119abc1f.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/3.119abc1f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55e50bf75b99e612d9f0",
+    "url": "/static/js/4.0c48ee00.chunk.js"
+  },
+  {
+    "revision": "c47bdfa29927ecc70d75",
+    "url": "/static/js/main.60b6bd1d.chunk.js"
+  },
+  {
+    "revision": "e4e766bb8b11c3d476f4",
+    "url": "/static/js/runtime-main.7f275f04.js"
+  },
+  {
+    "revision": "a92bb7b629c07dc6786ddf26952c2989",
+    "url": "/static/media/contact.a92bb7b6.png"
+  },
+  {
+    "revision": "29e82f1f846c831429f6566a7d69f5ec",
+    "url": "/static/media/digital_marketing_and_seo_final.29e82f1f.png"
+  },
+  {
+    "revision": "39b5f62fc978153651f226790b59c51e",
+    "url": "/static/media/fiestas_photobooth_site.39b5f62f.png"
+  },
+  {
+    "revision": "19c947743555b723c25b4759f47b60b7",
+    "url": "/static/media/fiestas_photobooth_site_mobile.19c94774.png"
+  },
+  {
+    "revision": "176f1b707573f5af5b5719d67d8dd696",
+    "url": "/static/media/home_design_final.176f1b70.png"
+  },
+  {
+    "revision": "f1c9247d0876f1d1d4f9015e0e83977e",
+    "url": "/static/media/julio_maldonado_site.f1c9247d.png"
+  },
+  {
+    "revision": "c92dbea3a1786cdb5b785059b91357e0",
+    "url": "/static/media/julio_maldonado_site_mobile.c92dbea3.png"
+  },
+  {
+    "revision": "1c66b6fdc59d5ea954c36f116317258e",
+    "url": "/static/media/love_yours_site.1c66b6fd.png"
+  },
+  {
+    "revision": "5e0f0c5baa1746e029c01c3b871aca5b",
+    "url": "/static/media/love_yours_site_mobile.5e0f0c5b.png"
+  },
+  {
+    "revision": "b068bfda47b0153302524fef08576d9f",
+    "url": "/static/media/real_time_analytics_v2.b068bfda.png"
+  },
+  {
+    "revision": "7dd9e7c4cb4e99b34985be9fd414eeb0",
+    "url": "/static/media/yeux_ground_up_final.7dd9e7c4.png"
+  },
+  {
+    "revision": "35f554a7fdaeb49ed48d23db250e90f8",
+    "url": "/static/media/yeux_site.35f554a7.png"
+  },
+  {
+    "revision": "46d21632147eb1eca85754a78219f3d5",
+    "url": "/static/media/yeux_site_mobile.46d21632.png"
+  },
+  {
+    "revision": "abd3b6dab2e047ac657f5cc341ff5eb3",
+    "url": "/static/media/yeux_story_final.abd3b6da.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yeux/precache-manifest.c47c3107a6142a4fc4ccc9c90ee0f0aa.js"
+  "/precache-manifest.f7b73f6cbc6afe598b352b98ada8bbe3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yeux/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
